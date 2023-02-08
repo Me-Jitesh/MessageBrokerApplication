e4df28252628c1d0ab3299d544b3e831ad2e2d2f
Jitesh Singh <Jiteshs101@gmail.com>
2023-02-08 10:35:03 +0530

    Changed Project Name

--- src/main/java/com/messagebroker/MessageBrokerApplication.java
+++ src/main/java/com/messagebroker/MessageBrokerApplication.java
@@ -1,13 +1,13 @@
-package com.p2pcommunication;
+package com.messagebroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P2PCommunicationApplication {
+public class MessageBrokerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P2PCommunicationApplication.class, args);
+		SpringApplication.run(MessageBrokerApplication.class, args);
 	}
 
 }
